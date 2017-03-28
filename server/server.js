d63b7d0b2549862edda81bf26f1b5679c64b64bf
alexyeah <fabian@unlv.nevada.edu>
2017-03-27 21:09:41 -0700

    working commit

--- server/server.js
+++ server/server.js
@@ -4,10 +4,6 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
 
-var database = require("./src/database");
-
-var userAccountsRef = database.ref("UserAccounts");
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('frontend/build'));
 
@@ -16,16 +12,14 @@ app.get('/', function(request, response) {
   console.log("responded to client")
 });
 
-io.on('connection',(socket)=>{
-  console.log('Client connected -  ' + socket.id)
-  //Receive messages from admin
-  socket.on('Admin', (data)=> {
-      //var content = fs.readFileSync(data,'utf8');
-      console.log("Message received: " + data);
+io.on('connection', (socket) => {
+  //Reply to application after receiving hello message
+  socket.on('hello', (msg)=> {
+      console.log("Message received: " + msg);
       socket.emit('reply', "hello from the server side");
   });
+  console.log("Client connected");
 });
-
 
 server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
