f518072b1f25a30fd229a00027dfeb511fe2e505
Martin Jaime <martin.jaime-viveros@originate.com>
2017-03-29 15:02:48 -0700

    Fix bug where client didn't use proper host name

--- server/frontend/src/containers/App.js
+++ server/frontend/src/containers/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket = SocketIOClient(location.origin);
     this.keyPressEvent = this.keyPressEvent.bind(this);
   }
 
