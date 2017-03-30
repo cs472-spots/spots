fd83b5fee1abf13955ab30868ac1d82ad7b19fec
alexyeah <fabian@unlv.nevada.edu>
2017-03-29 20:51:17 -0700

    added register function -- includes user permit and vehicle

--- server/server.js
+++ server/server.js
@@ -3,52 +3,17 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-
 var database = require("./src/database");
-var spotsHW = require("./routes/spotsHW")
+var spotsHW = require("./routes/spotsHW");
+var Enum = require('enum');
+
+var permitType = new Enum(['none', 'Student', 'Faculty']);
+var None = permitType.get(1);
+var Student = permitType.get(2);
+var Faculty = permitType.get(3);
 
 var userAccountsRef = database.ref("UserAccounts");
 
-function register(userId, first, last, email, gotPermit){
-  var userIDRef = database.ref('UserAccounts/' + userId);
-  userIDRef.set({
-    First_Name: first,
-    Last_Name: last,
-    Email: email,
-    Permit: gotPermit
-  });
-  /*
-  if(gotPermit){
-    userIDRef.ref('Permit').set({
-      isExpired: no,
-      ExpDate: 3-29-2017,
-      Permit_Type: Student
-    });
-  }
-  else{
-    userIDRef.ref('Permit').set({
-      isExpired: null,
-      ExpDate: null,
-      Permit_Type: none,
-    });
-  }*/
-}
-
-register(1000584, "Alexies", "Fabian", "fabian@unlv.nevada.edu", true);
-register(1234567, "Who", "Dis", "newphone_whodis@yahoo.com", false);
-/*
-userAccountsRef.child(key).set(userID);
-
-//then you can write in that node in this way
-function writeUserData(userId, name, email) {
-  database.ref('UserAccount' + userID).set({
-  nshe: userID,
-  'nshe/name': name,
-  'nshe/email': email
-  });
-}
-
-writeUserData(userID, name, email);*/
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('frontend/build'));
 
@@ -72,3 +37,51 @@ io.on('connection', (socket) => {
 server.listen(app.get('port'), function() {
   console.log("Spots server is running on http://localhost:%s", app.get('port'));
 });
+
+//--------------------------------------------------------------------------------------------
+//Functions
+//--------------------------------------------------------------------------------------------
+
+//Registers User to database
+//Paramters: (int, string, string, string, int, boolean, boolean, string?, permitType,
+//  string?, string?, string?, string?)
+function registerUser(userId, first, last, email, phone, gotPermit, expired, expDate,
+  permittype, make, model, color, plate){
+  var userIDRef = database.ref('UserAccounts/' + userId);
+  userIDRef.set({
+    First_Name: first,
+    Last_Name: last,
+    Email: email,
+    Phone: phone,
+    Permit: gotPermit
+  });
+
+  if(gotPermit){
+    database.ref('UserAccounts/' + userId + '/Permit').set({
+      isExpired: expired,
+      ExpDate: expDate,
+      Permit_Type: permittype
+    });
+
+    database.ref('UserAccounts/' + userId + '/Vehicle').set({
+      Make: make,
+      Model: model,
+      Color: color,
+      Plate_Number: plate
+    });
+  }
+  else{
+    database.ref('UserAccounts/' + userId + '/Permit').set({
+      isExpired: null,
+      ExpDate: null,
+      Permit_Type: None
+    });
+
+    database.ref('UserAccounts/' + userId + '/Vehicle').set({
+      Make: null,
+      Model: null,
+      Color: null,
+      Plate_Number: null
+    });
+  }
+}
