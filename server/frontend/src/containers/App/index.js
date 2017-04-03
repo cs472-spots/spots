8cf5b5d39de893a2eefb664b664382e35ba05816
Martin Jaime <martin.jaime-viveros@originate.com>
2017-04-03 15:19:38 -0700

    Remove whitespace replace tabs with spaces

--- server/frontend/src/containers/App/index.js
+++ server/frontend/src/containers/App/index.js
@@ -4,15 +4,15 @@ import classnames from 'classnames';
 import './App.css';
 
 class App extends Component {
-  
+
   render() {
     const { className, ...props } = this.props;
     return (
       <div className={classnames('App', className)} {...props}>
-      	<div className="content">
+        <div className="content">
           <h2>Hello from App</h2>
-		    </div>
-	    </div>
+        </div>
+      </div>
     );
   }
 }
--- server/frontend/src/containers/Register/index.js
+++ server/frontend/src/containers/Register/index.js
@@ -4,15 +4,15 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 
 class Register extends Component {
-	render () {
-		const { className, ...props } = this.props;
-		
-		return (
-			<div className={classnames('Register', className)} {...props}>
-				<h2>Hello from Register!</h2>
-			</div>
-		);
-	}
+  render () {
+    const { className, ...props } = this.props;
+
+    return (
+      <div className={classnames('Register', className)} {...props}>
+        <h2>Hello from Register!</h2>
+      </div>
+    );
+  }
 }
 
 export default Register;
