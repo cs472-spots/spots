f6dd42b76cedcb16116f2eb1d0322336921f36ac
nhand2 <nhand2@unlv.nevada.edu>
2017-04-02 23:29:35 -0700

    Adds react-router

---
+++ server/frontend/src/containers/App/index.js
@@ -0,0 +1,20 @@
+// src/containers/App
+import React, { PropTypes, Component } from 'react';
+import classnames from 'classnames';
+import './App.css';
+
+class App extends Component {
+  
+  render() {
+    const { className, ...props } = this.props;
+    return (
+      <div className={classnames('App', className)} {...props}>
+      	<div className="content">
+          <h2>Hello from App</h2>
+		    </div>
+	    </div>
+    );
+  }
+}
+
+export default App;
