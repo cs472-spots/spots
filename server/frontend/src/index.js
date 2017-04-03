337d2090ba2aaed02c7295c17bd946f7485b6c75
nhand2 <nhand2@unlv.nevada.edu>
2017-04-03 14:39:55 -0700

    Added missing files for react-router

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -1,9 +1,18 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import { browserHistory, hashHistory } from 'react-router';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
 import App from './containers/App';
+
+import Routes from './routes/index.js';
+
 import './index.css';
 
+//Renders the components passed by 
 ReactDOM.render(
-  <App />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
