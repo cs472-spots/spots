70fd0de5d8df17cbfd2436b58fa6dd13396f7016
nhand2 <nhand2@unlv.nevada.edu>
2017-04-05 15:17:01 -0700

    Adds nectarine and modify file names

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -1,15 +1,19 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { browserHistory } from 'react-router';
+import { Provider } from 'nectarine';
 // eslint-disable-next-line
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
+import Store from './store/Store.js';
 import Routes from './routes/Routes.js';
 
+import './index.css';
 //Renders the components passed by 
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+  <Provider store={store}>
+    <Routes history={browserHistory} />
+  </Provider>
   document.getElementById('root')
 );
