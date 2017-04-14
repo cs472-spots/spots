cb2adc6865bbe163ecc3b42699aceb5dc0bd6d97
Derek Nhan <nhand2@unlv.nevada.edu>
2017-04-13 18:19:02 -0700

    Webapp (#31)
    * Adds Auth0
    
    * Fixed Auth0 reroute to home
    
    * Added sidebar functionality
    
    * Removed npm-debug.log and .DS_Store

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -1,19 +1,18 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'nectarine';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
+import { Provider } from 'nectarine';
 // eslint-disable-next-line
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
-import './index.css';
-import store from './store';
+import Store from './store/Store.js';
 import Routes from './routes/index.js';
+import './index.css';
 
 //Renders the components passed by
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes history={browserHistory} />
+  <Provider store={Store}>
+    <Routes history={hashHistory} />
   </Provider>,
   document.getElementById('root')
 );
