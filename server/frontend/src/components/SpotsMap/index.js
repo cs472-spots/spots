1086ec2c8cab0379ff0b33cfb1467a816f74fbfd
Martin Jaime <martin.jaime-viveros@originate.com>
2017-04-23 22:54:09 -0700

    renders sopts from an array of spots

--- server/frontend/src/components/SpotsMap/index.js
+++ server/frontend/src/components/SpotsMap/index.js
@@ -6,9 +6,26 @@ import Spot from './Spot';
 
 class SpotsMap extends Component {
   static defaultProps = {
-    center: {lat: 36.111669, lng: -115.141018},
+    center: {lat: 36.106992, lng: -115.143744},
     zoom: 19
   };
+
+  renderSpots = () => {
+    return [(
+      <Spot
+        lat={36.106992}
+        lng={-115.143744}
+        key='1'
+      />
+    ),
+    (
+      <Spot
+        lat={36.106795}
+        lng={-115.143744}
+        key='2'
+      />
+    )]
+  }
 
   render() {
     return (
@@ -17,10 +34,7 @@ class SpotsMap extends Component {
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
       >
-        <Spot
-          lat={36.111669}
-          lng={-115.141018}
-        />
+      {this.renderSpots()}
       </GoogleMapReact>
     )
   }
