348c1dc68847e88e1e466c0816eb814c80b8d1ed
Martin Jaime <martin.jaime-viveros@originate.com>
2017-04-29 00:47:49 -0700

    Fix database constructor
    have database constructor create url based on environment variable

--- server/src/database.js
+++ server/src/database.js
@@ -1,9 +1,13 @@
 var ServiceAccount = require("../firebase-private-key.js");
 var admin = require("firebase-admin");
 
+function getFbURL() {
+  return "https://" + process.env.FIREBASE_PROJECT_ID + ".firebaseio.com";
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(ServiceAccount),
-  databaseURL: "https://spots-cba38.firebaseio.com"
+  databaseURL: getFbURL()
 });
 
 module.exports = admin.database()
