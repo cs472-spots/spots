8e763303f26a87966917fde6737c573e74bf8dca
Martin Jaime <martin.jaime-viveros@originate.com>
2017-04-28 22:22:45 -0700

    modularize spot styles

--- server/frontend/src/components/SpotsMap/Spot.js
+++ server/frontend/src/components/SpotsMap/Spot.js
@@ -1,30 +1,9 @@
 import React from 'react';
-
-const K_WIDTH = 3;
-const K_HEIGHT = K_WIDTH * 3;
-
-const greatPlaceStyle = {
-  // initially any map object has left top corner at lat lng coordinates
-  // it's on you to set object origin to 0,0 coordinates
-  position: 'absolute',
-  width: K_WIDTH,
-  height: K_HEIGHT,
-  left: -K_WIDTH / 2,
-  top: -K_HEIGHT / 2,
-
-  border: '5px solid #f44336',
-  //borderRadius: K_HEIGHT,
-  backgroundColor: 'transparent',
-  textAlign: 'center',
-  color: '#3f51b5',
-  fontSize: 16,
-  fontWeight: 'bold',
-  padding: 4
-};
+import Styles from './Styles';
 
 const Spot = ({text}) => {
   return (
-    <div style={greatPlaceStyle}>
+    <div style={Styles.notVacant}>
       {text}
     </div>
   )
