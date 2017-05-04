50eed1ad8a0cac37208e1bdb4d2613926ad72150
Derek Nhan <nhand2@unlv.nevada.edu>
2017-05-04 01:09:32 -0700

    Updated to show nothing when no notifications

--- server/frontend/src/components/SideBar/index.js
+++ server/frontend/src/components/SideBar/index.js
@@ -50,7 +50,7 @@ class SideBar extends React.Component {
             <a href="#/app/monitor_spots" className="sidebar-menu-item">
               <i className="fa fa-automobile"></i>
               <span className="sidebar-menu-item-text">Monitor Spots</span>
-              <div className={(this.props.notifications > 0) ? 'badge-num' : null}>{this.props.notifications}</div>
+              <div className={(this.props.notifications > 0) ? 'badge-num' : null}>{this.props.notifications>0 ? this.props.notifications : null}</div>
             </a>
           </li>
           <li>
